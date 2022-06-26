4b15f56ad37bc1bbe3db1188c5083daaf7ce33d5
KacpiDrewniak <k.drewniak.dev@gmail.com>
2022-06-26 16:30:12 +0200

    auth0 works

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { loginWithRedirect, logout, isAuthenticated, isLoading, user } =
+    useAuth0();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => logout({ returnTo: window.location.origin })}>
+        Log Out
+      </button>
+      <button onClick={() => loginWithRedirect()}>Log In</button>
+      asd
+      <>
+        {!isLoading && isAuthenticated && (
+          <div>
+            <img src={user?.picture} alt={user?.name} />
+            <h2>{user?.name}</h2>
+            <p>{user?.email}</p>
+          </div>
+        )}
+      </>
     </div>
   );
 }
